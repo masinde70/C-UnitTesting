ce3414e2333e419456c3fcd113ee8536f438592f
masinde70 <masinde70@gmail.com>
2020-10-01 13:11:34 +0300

    unit_testing with c++ first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/masinde/cppt_test/Testing/UNitTesting
+# Build directory: /home/masinde/cppt_test/Testing/UNitTesting/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(catch_test "/home/masinde/cppt_test/Testing/UNitTesting/build/cpp_test" "--success")
+set_tests_properties(catch_test PROPERTIES  _BACKTRACE_TRIPLES "/home/masinde/cppt_test/Testing/UNitTesting/CMakeLists.txt;25;add_test;/home/masinde/cppt_test/Testing/UNitTesting/CMakeLists.txt;0;")
