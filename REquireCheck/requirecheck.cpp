1a7f7bdae5ca21d9574702dc8598f325c104b683
masinde70 <masinde70@gmail.com>
2020-10-21 11:44:02 +0300

    require_check_one

--- REquireCheck/requirecheck.cpp
+++ REquireCheck/requirecheck.cpp
@@ -1,5 +1,46 @@
 #include<catch2/catch.hpp>
 
-std string::one(){
+std::string one(){
+   return "1"; 
+}
+
+TEST_CASE("Assert that something is true(pass)", "[require]"){
+    REQUIRE( one() == "1");
+}
+
+TEST_CASE("Assert that something is true(fail)", "[require]"){
+    REQUIRE( one() == "x");
+}
+
+TEST_CASE("Assert that something is true (stop at the first fail)", "[require]"){
+    WARN( "REQUIRE stop at first failure:" );
+
+    REQUIRE( one() == "x");
+    REQUIRE( one() == "1");
+}
+
+
+TEST_CASE(""){
     
 }
+
+
+TEST_CASE(""){
+    
+}
+
+
+TEST_CASE(""){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
